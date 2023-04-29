df84076f6ab8619b4657a5081d86a82439e5b4ea
Alimzhanov456 <82152836+Alimzhanov456@users.noreply.github.com>
2023-04-29 13:36:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from './RegistrationForm.module.css';
 
-function App() {
+const RegistrationForm = () => {
+  const [fullName, setFullName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleFullNameChange = (event) => {
+    setFullName(event.target.value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Full Name: ${fullName}\nPhone Number: ${phoneNumber}\nEmail: ${email}`);
+    alert(`Full Name: ${fullName}\nPhone Number: ${phoneNumber}\nEmail: ${email}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <form className={styles.registrationForm} onSubmit={handleSubmit}>
+      <label className={styles.formLabel}>
+        Full Name:
+        <input
+          className={styles.formInput}
+          type="text"
+          value={fullName}
+          onChange={handleFullNameChange}
+        />
+      </label>
 
-export default App;
+      <label className={styles.formLabel}>
+        Phone Number:
+        <input
+          className={styles.formInput}
+          type="tel"
+          value={phoneNumber}
+          onChange={handlePhoneNumberChange}
+        />
+      </label>
+
+      <label className={styles.formLabel}>
+        Email:
+        <input
+          className={styles.formInput}
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </label>
+
+      <button className={styles.formButton} type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
+
+
